c4fdaa2f0d68c6d9e1ef86399e14f66e5ffcec32
José Alvarez <josealvarez712@gmail.com>
2016-10-11 23:18:27 -0500

    paso 3 test para probar el detalle (rojo)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -57,3 +57,13 @@ class FuncionalTest(TestCase):
         span = self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
 
         self.assertIn('Juan Daniel Arevalo', span.text)
+
+
+    def test_verDetalle(self):
+        self.browser.get('http://localhost:8000')
+        span = self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
+        span.click()
+
+        h2 = self.browser.find_element(By.XPATH, '//h2[text()="Juan Daniel Arevalo"]')
+
+        self.assertIn('Juan Daniel Arevalo', h2.text)
